08c4be1af030cdda35dd8612ec4955aac4c9c9ed
WaffleSquirrel <rockabilly@outlook.com>
2015-12-07 23:37:54 -0800

    Adding a couple [File System] utility nuggets.
    1. Added a PowerShell script that will copy all contents from a directory to another destination.
    2. Added a PowerShell script that will auto-generate a bunch of directory folders based on a range of ASCII characters.

---
+++ nugs/powershell-scripts/[File System]-Copy-All-Directory-Contents.ps1
@@ -0,0 +1,6 @@
+﻿# Copy all folder contents that start with the letter B
+
+$fromHere = "\\SHINY-PC\Users\Me\Music\iTunes\iTunes Media\Music\B*"
+$toHere = "C:\Users\Elvis\Music"
+
+Copy-Item -Path ($fromHere) -Destination ($toHere) -Force -Recurse
---
+++ nugs/powershell-scripts/[File System]-Create-Directories-With-Letters-From-Alphabet.ps1
@@ -0,0 +1,26 @@
+﻿# Some common chars used for file/directory names
+# ------------------------------------------------
+# 0-9 ascii codes -> 48 to 57
+# A-Z ascii codes -> 65 to 90
+# a-z ascii codes -> 97 to 122
+# ( ascii code -> 40
+# ) ascii code -> 41
+# - ascii code -> 45
+# . ascii code -> 46
+# [ ascii code -> 91
+# ] ascii code -> 93
+# _ ascii code -> 95
+
+# Print out 0-9 ascii chars
+#48..57 | % {“[ASCII Character Code #{0}] = {1}” –f $_, [char]$_}
+
+# Print out A-Z ascii chars
+#65..90 | % {“[ASCII Character Code #{0}] = {1}” –f $_, [char]$_}
+
+# Print out a-z ascii chars
+#97..122 | % {“[ASCII Character Code #{0}] = {1}” –f $_, [char]$_}
+
+
+# Create a directory for each letter in A-Z
+# Note that md is the alias for mkdir
+65..90 | % {md ("{0}" -f [char]$_)}
