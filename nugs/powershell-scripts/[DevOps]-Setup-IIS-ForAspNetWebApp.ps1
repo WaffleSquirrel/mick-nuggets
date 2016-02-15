175ac8fc152cc437fef0373c12b1b2106b03a1b3
WaffleSquirrel <rockabilly@outlook.com>
2016-02-14 22:42:16 -0800

    Add a PowerShell [DevOps] nugget that installs the base Windows Features for hosting an ASP.NET web app on IIS.

---
+++ nugs/powershell-scripts/[DevOps]-Setup-IIS-ForAspNetWebApp.ps1
@@ -0,0 +1,14 @@
+﻿Import-Module ServerManager
+
+# To list the feature names
+#Get-WindowsFeature
+
+# For a single feature and rebooting after
+#Add-WindowsFeature SomeFeatureName -restart
+
+# For multiple features at once
+#Add-WindowsFeature Web-WebServer,NET-Framework-45-ASPNET,Web-Mgmt-Tools
+
+
+# Enable IIS Features
+Add-WindowsFeature Web-WebServer,NET-Framework-45-ASPNET,Web-Mgmt-Tools
